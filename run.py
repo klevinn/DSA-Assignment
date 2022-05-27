13a7b6f98fc804daebd1167463ac43c70db8486d
calpis <calvinlyn11@gmail.com>
2022-05-27 22:29:25 +0800

    delete

--- run.py
+++
@@ -1,9 +0,0 @@
-from main import main
-
-# I am Calvin Lai, 212775C from Nanyang Polytechnic: Cyber Security Course.
-# This is my Data Structure & Algorithms Project. It consists of a Sorting System & a Search System.
-
-# Features Included : Bubble Sort, Selection Sort, Insertion Sort, Quick Sort, Merge Sort, Binary Search, Linear Search , Counting Sort, Pagination (WIP), Table Algorithm (WIP)
-
-if __name__ == "__main__":
-    main()
